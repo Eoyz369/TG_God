c1bdd6bc55c4038925a133caa7e335aceabd192f
Eoyz369 <113827000+Eoyz369@users.noreply.github.com>
2023-04-19 20:50:07 +0800

    Update TGPlus.java

--- TGPlus/app/src/main/java/com/eoyz369/tgplus/TGPlus.java
+++ TGPlus/app/src/main/java/com/eoyz369/tgplus/TGPlus.java
@@ -1,30 +1,60 @@
 package com.eoyz369.tgplus;
 
-import android.util.Log;
+// 导入Xposed相关的类
 
-import java.util.Arrays;
-import java.util.List;
+import android.app.AndroidAppHelper;
+
+import java.util.HashSet;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
-public class TGPlus implements IXposedHookLoadPackage {
 
-    //声明
-    //HOOK 包名
-    private static final List<String> hookPackages = Arrays.asList("org.telegram.messenger", "org.telegram.messenger.web");
+
+public class TestHOOk implements IXposedHookLoadPackage {
+
+
+    private static final HashSet<String> PACKAGE_NAMES = new HashSet<>();
+
+    static {
+        // 定义一个包名列表，用于过滤目标应用程序
+        PACKAGE_NAMES.add("org.telegram.messenger");
+        PACKAGE_NAMES.add("org.telegram.messenger.web");
+        PACKAGE_NAMES.add("org.telegram.messenger.beta");
+    }
+
 
     @Override
-    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lPParam) throws Throwable {
-        if (hookPackages.contains(lPParam.packageName)) {
-            //HOOK Telegram Premium
-            //HOOK 电报 会员
-            XposedHelpers.findAndHookMethod("org.telegram.messenger.UserConfig", lPParam.classLoader, "isPremium", XC_MethodReplacement.returnConstant(true));
-
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        // 获取当前的包名
+        String packageName = AndroidAppHelper.currentPackageName();
+        // 判断当前的包名是否在集合中
+        if (!PACKAGE_NAMES.contains(packageName)) {
+            return;
         }
+
+        // 打印日志信息，显示当前进程是目标应用程序
+        XposedBridge.log("【current process is target app: " + packageName + "】");
+
+        // 在这里编写你想要执行的代码，例如 hook 某个方法或者修改某个变量等等
+        // ...
+        
+        /**
+         *     HOOK Telegram Local Premium
+         *     HOOK 电报 本地会员
+         *     unlockPremium
+         */
+
+        XposedHelpers.findAndHookMethod("org.telegram.messenger.UserConfig", lpparam.classLoader, "isPremium", XC_MethodReplacement.returnConstant(true));
+        XposedBridge.log("【HOOK Telegram Local Premium Sucess】 ");
+
+
     }
+
+
 }
+
+
